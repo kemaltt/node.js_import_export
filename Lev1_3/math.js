ea896fb65e2c5122be5ec4e4127b70ccd8e5ce29
kemaltt <ttnckemal@gmail.com>
2022-06-29 23:54:49 +0200

    eval

--- Lev1_3/math.js
+++ Lev1_3/math.js
@@ -1,15 +1,25 @@
 const math = (a, b, operator) => {
-  switch (operator) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return a / b;
-    default:
-      return "error";
+  // switch (operator) {
+  //   case "+":
+  //     return a + b;
+  //   case "-":
+  //     return a - b;
+  //   case "*":
+  //     return a * b;
+  //   case "/":
+  //     return a / b;
+  //   default:
+  //     return "error";
+  // }
+  if (
+    operator === "+" ||
+    operator === "-" ||
+    operator === "*" ||
+    operator === "/"
+  ) {
+    return eval(a + operator + b);
+  } else {
+    return "error";
   }
 };
 
